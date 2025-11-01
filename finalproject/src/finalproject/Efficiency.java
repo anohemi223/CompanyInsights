f258a6f3996f8119d19f54f26366f8335b88046a
anohemi223 <anohemi223@gmail.com>
2025-10-31 23:05:12 -0700

    Add Efficiency class with turnover ratios

--- finalproject/src/finalproject/Efficiency.java
+++ finalproject/src/finalproject/Efficiency.java
@@ -1,1 +1,52 @@
+public class Efficiency extends Metrics {
+    private double assetTurnover;
+    private double inventoryTurnover;
 
+    /**
+     * Constructs an Efficiency object.
+     * 
+     * @param assetTurnover     the asset turnover ratio
+     * @param inventoryTurnover the inventory turnover ratio
+     */
+    public Efficiency(double assetTurnover, double inventoryTurnover) {
+        super(null); // no FinancialInformation[] provided
+        this.assetTurnover = assetTurnover;
+        this.inventoryTurnover = inventoryTurnover;
+    }
+
+    /**
+     * TODO
+     * 
+     * @return the asset turnover ratio
+     */
+    public double getAssetTurnover() {
+        return assetTurnover;
+    }
+
+    /**
+     * TODO
+     * 
+     * @param assetTurnover the new asset turnover ratio
+     */
+    public void setAssetTurnover(double assetTurnover) {
+        this.assetTurnover = assetTurnover;
+    }
+
+    /**
+     * TODO
+     * 
+     * @return the inventory turnover ratio
+     */
+    public double getInventoryTurnover() {
+        return inventoryTurnover;
+    }
+
+    /**
+     * TODO
+     * 
+     * @param inventoryTurnover the new inventory turnover ratio
+     */
+    public void setInventoryTurnover(double inventoryTurnover) {
+        this.inventoryTurnover = inventoryTurnover;
+    }
+}
