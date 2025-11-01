3623b9b32d654a7c6ce72a297e0c4ec506c5b69f
anohemi223 <anohemi223@gmail.com>
2025-10-31 23:06:25 -0700

    Implement financial information management in Metrics
    Added constructor and getter/setter methods for financial information.

--- finalproject/src/finalproject/Metrics.java
+++ finalproject/src/finalproject/Metrics.java
@@ -2,5 +2,32 @@
  * 
  */
 public class Metrics {
+    private FinancialInformation[] financialInformation;
 
+    /**
+     * 
+     * 
+     * @param financialInformation an array of FinancialInformation objects
+     */
+    public Metrics(FinancialInformation[] financialInformation) {
+        this.financialInformation = financialInformation;
+    }
+
+    /**
+     * Returns the financial information array.
+     * 
+     * @return the array of FinancialInformation 
+     */
+    public FinancialInformation[] getFinancialInformation() {
+        return financialInformation;
+    }
+
+    /**
+     * Sets the financial information array.
+     * 
+     * @param financialInformation the new array of FinancialInformation 
+     */
+    public void setFinancialInformation(FinancialInformation[] financialInformation) {
+        this.financialInformation = financialInformation;
+    }
 }
