592020934534837639775c72b7be22c813e040c6
anohemi223 <anohemi223@gmail.com>
2025-10-31 23:06:02 -0700

    Implement Liquidity class with ratio getters and setters
    Added Liquidity class with current and quick ratio methods.

--- finalproject/src/finalproject/Liquidity.java
+++ finalproject/src/finalproject/Liquidity.java
@@ -1,1 +1,52 @@
+public class Liquidity extends Metrics {
+    private double currentRatio;
+    private double quickRatio;
 
+    /**
+     * Constructs a Liquidity object.
+     * 
+     * @param currentRatio the current ratio
+     * @param quickRatio   the quick ratio
+     */
+    public Liquidity(double currentRatio, double quickRatio) {
+        super(null); // no FinancialInformation[] provided
+        this.currentRatio = currentRatio;
+        this.quickRatio = quickRatio;
+    }
+
+    /**
+     * Returns the current ratio.
+     * 
+     * @return the current ratio
+     */
+    public double getCurrentRatio() {
+        return currentRatio;
+    }
+
+    /**
+     * Sets the current ratio.
+     * 
+     * @param currentRatio the new current ratio
+     */
+    public void setCurrentRatio(double currentRatio) {
+        this.currentRatio = currentRatio;
+    }
+
+    /**
+     * Returns the quick ratio.
+     * 
+     * @return the quick ratio
+     */
+    public double getQuickRatio() {
+        return quickRatio;
+    }
+
+    /**
+     * Sets the quick ratio.
+     * 
+     * @param quickRatio the new quick ratio
+     */
+    public void setQuickRatio(double quickRatio) {
+        this.quickRatio = quickRatio;
+    }
+}
