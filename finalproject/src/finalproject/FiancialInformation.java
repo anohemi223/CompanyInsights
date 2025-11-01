1c82dce2c7a497c99457f1a432642dff3bea2fc4
anohemi223 <anohemi223@gmail.com>
2025-10-31 23:05:40 -0700

    Create FinancialInformation class with attributes and methods
    Added FinancialInformation class with properties and methods for financial data management.

--- finalproject/src/finalproject/FiancialInformation.java
+++ finalproject/src/finalproject/FiancialInformation.java
@@ -1,1 +1,81 @@
+/**
+ * 
+ */
+
+public class FinancialInformation {
+    private String assets;
+    private String liabilities;
+    private double revenue;
+    private int cost;
+    private int netIncome;
+    private int period;
+
+    /**
+     * Constructs a FinancialInformation object with the given values.
+     * 
+     * @param assets       
+     * @param liabilities  
+     * @param revenue      
+     * @param cost         
+     * @param netIncome  
+     * @param period       
+     */
+    public FinancialInformation(String assets, String liabilities, double revenue, int cost, int netIncome, int period) {
+        this.assets = assets;
+        this.liabilities = liabilities;
+        this.revenue = revenue;
+        this.cost = cost;
+        this.netIncome = netIncome;
+        this.period = period;
+    }
+
+    // Getters and setters
+    public String getAssets() {
+        return assets;
+    }
+
+    public void setAssets(String assets) {
+        this.assets = assets;
+    }
+
+    public String getLiabilities() {
+        return liabilities;
+    }
+
+    public void setLiabilities(String liabilities) {
+        this.liabilities = liabilities;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getNetIncome() {
+        return netIncome;
+    }
+
+    public void setNetIncome(int netIncome) {
+        this.netIncome = netIncome;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+}
 
