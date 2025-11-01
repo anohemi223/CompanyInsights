1d7132a167eeeaec8b72b4d5291673ae1717dced
anohemi223 <anohemi223@gmail.com>
2025-10-31 23:07:46 -0700

    Enhance User class with report and GUI launch method
    Added report field and launchReportGUI method to User class.

--- finalproject/src/finalproject/User.java
+++ finalproject/src/finalproject/User.java
@@ -5,6 +5,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.SwingUtilities;
+
 /**
  * 
  */
@@ -12,6 +14,7 @@ public class User extends Company{
 	private String name;
 	private String email;
 	private List<Company> companies;
+	private Object report;
 	// Constructor for creating a user with a name and email
 	private User(String name, String email) {
 		this.name = name;
@@ -45,5 +48,12 @@ public class User extends Company{
 		this.companies = companies;
 	}
 
+	 public void launchReportGUI() {
+	        SwingUtilities.invokeLater(() -> {
+	            Report gui = new Report(this.report);
+	            gui.setVisible(true);
+	        });
+	    }
 
 }
+
