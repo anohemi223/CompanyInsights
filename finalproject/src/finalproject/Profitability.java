3ea6fed62907cf307cc143580d71e1e931312bfc
anohemi223 <anohemi223@gmail.com>
2025-10-31 23:06:49 -0700

    Implement Profitability class with margin methods
    Added Profitability class to manage gross and operating margins.

--- finalproject/src/finalproject/Profitability.java
+++ finalproject/src/finalproject/Profitability.java
@@ -1,1 +1,53 @@
+public class Profitability extends Metrics {
+    private double grossMargin;
+    private double operatingMargin;
 
+    /**
+     * Constructs a Profitability object.
+     * 
+     * @param grossMargin      
+     * @param operatingMargin  
+     * @param Metrics 
+     */
+    public Profitability(double grossMargin, double operatingMargin, FinancialInformation[] Metrics) {
+        super(Metrics); // TODO fix??
+        this.grossMargin = grossMargin;
+        this.operatingMargin = operatingMargin;
+    }
+
+    /**
+     * Returns the gross margin.
+     * 
+     * @return the gross margin
+     */
+    public double getGrossMargin() {
+        return grossMargin;
+    }
+
+    /**
+     * Sets the gross margin.
+     * 
+     * @param grossMargin the new gross margin
+     */
+    public void setGrossMargin(double grossMargin) {
+        this.grossMargin = grossMargin;
+    }
+
+    /**
+     * Returns the operating margin.
+     * 
+     * @return the operating margin
+     */
+    public double getOperatingMargin() {
+        return operatingMargin;
+    }
+
+    /**
+     * Sets the operating margin.
+     * 
+     * @param operatingMargin the new operating margin
+     */
+    public void setOperatingMargin(double operatingMargin) {
+        this.operatingMargin = operatingMargin;
+    }
+}
