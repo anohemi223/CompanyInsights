f89ee076fb51aa5b41d5b3a745a1670db94e98c7
anohemi223 <anohemi223@gmail.com>
2025-12-05 23:00:31 -0800

    Enhance TradingCard class with JavaDoc comments
    Added JavaDoc comments to the TradingCard class and its methods for better documentation.

--- finalproject/src/finalproject/TradingCard.java
+++ finalproject/src/finalproject/TradingCard.java
@@ -1,32 +1,56 @@
 /**
- * 
- */
-
-/**
- * 
+ * Abstract class representing a generic trading card.
+ * Contains basic information like the card's name and collector number.
  */
 public abstract class TradingCard {
+
     private String name;
     private int collectorNumber;
 
+    /**
+     * Constructs a TradingCard with the given name and collector number.
+     *
+     * @param name the name of the card
+     * @param collectorNumber the collector number of the card
+     */
     public TradingCard(String name, int collectorNumber) {
         this.name = name;
         this.collectorNumber = collectorNumber;
     }
 
-	    
-	    public abstract String getCategory();
+    /**
+     * Returns the category of the trading card.
+     * Implemented by subclasses like PokemonCard and POkemonDisplayOnlyCard
+     *
+     * @return the category of the card as a String
+     */
+    public abstract String getCategory();
 
-	    public String getName() {
-	        return name;
-	    }
+    /**
+     * Returns the name of the card
+     *
+     * @return the card's name
+     */
+    public String getName() {
+        return name;
+    }
 
-	    public int getCollectorNumber() {
-	        return collectorNumber;
-	    }
+    /**
+     * Returns the collector number of the card
+     *
+     * @return the collector number
+     */
+    public int getCollectorNumber() {
+        return collectorNumber;
+    }
 
-	    @Override
-	    public String toString() {
-	        return name + " (#" + collectorNumber + ")";
-	    }
-	}
+    /**
+     * Returns a string representation of the card.
+     *
+     * @return the card's name followed by its collector number
+     */
+    @Override
+    public String toString() {
+        return name + " (#" + collectorNumber + ")";
+    }
+}
