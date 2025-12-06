c587242a77d5b8eeb40e8f404fd6b5ad4c077a4a
anohemi223 <anohemi223@gmail.com>
2025-12-05 22:57:18 -0800

    Enhance CollectionManager with trainer details
    Added functionality to manage trainer information and updated card management methods.

--- finalproject/src/finalproject/CollectionManager.java
+++ finalproject/src/finalproject/CollectionManager.java
@@ -12,35 +12,85 @@ public class CollectionManager {
 
 	private List<PokemonCard> collection;
 
-	public CollectionManager() {
-		this.collection = new ArrayList<>();
-	}
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
-	public List<PokemonCard> getCollection() {
-		return collection;
-	}
+/**
+ * Manages a collection of Pokémon cards for a trainer.
+ * Provides functionality to add cards, retrieve the collection, and display it.
+ */
+public class CollectionManager {
 
-	public void addCard(PokemonCard card) {
-		for (int i = 0; i < collection.size(); i++) {
-			PokemonCard existingCard = collection.get(i);
+    private List<PokemonCard> collection;
+    private String trainerName;
+    private String trainerFavPokemon;
 
-			// Compare both name and collector number
-			if (existingCard.getName().equalsIgnoreCase(card.getName())) {
-				// Check card stats for matching collectorNumber
-				for (CardStats newStats : card.getStats()) {
-					existingCard.addOrUpdateStats(newStats);
-				}
-				return;
-			}
-		}
+    /**
+     * Default constructor initializes an empty collection.
+     */
+    public CollectionManager() {
+        this.collection = new ArrayList<>();
+    }
 
-		// If no matching card name/collector number combo found, add new card
-		collection.add(card);
-	}
+    /**
+     * Constructor initializes a collection for a trainer with a given name.
+     * 
+     * @param trainerName Name of the trainer
+     */
+    public CollectionManager(String trainerName) {
+        this.trainerName = trainerName;
+        this.collection = new ArrayList<>();
+    }
 
-	public void displayCollection() {
-		for (PokemonCard card : collection) {
-			System.out.println(card);
-		}
-	}
+    /**
+     * Returns the list of Pokémon cards in the collection.
+     * 
+     * @return List of PokemonCard objects
+     */
+    public List<PokemonCard> getCollection() {
+        return collection;
+    }
+
+    /**
+     * Adds a Pokémon card to the collection.
+     * If a card with the same name already exists, its stats are updated instead.
+     * 
+     * @param card The PokemonCard to add or update
+     */
+    public void addCard(PokemonCard card) {
+        for (int i = 0; i < collection.size(); i++) {
+            PokemonCard existingCard = collection.get(i);
+
+            // Compare both name and collector number
+            if (existingCard.getName().equalsIgnoreCase(card.getName())) {
+                // Check card stats for matching collectorNumber
+                for (CardStats newStats : card.getStats()) {
+                    existingCard.addOrUpdateStats(newStats);
+                }
+                return;
+            }
+        }
+
+        // If no matching card name/collector number combo found, add new card
+        collection.add(card);
+    }
+
+    /**
+     * Prints the Pokémon collection
+     */
+    public void displayCollection() {
+        for (PokemonCard card : collection) {
+            System.out.println(card);
+        }
+    }
+
+    /**
+     * Returns the trainer's name.
+     * 
+     * @return trainerName
+     */
+    public String getTrainerName() {
+        return trainerName; 
+    }
 }
