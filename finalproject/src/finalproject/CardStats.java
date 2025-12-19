e4bf88e8ad775202e268ce2a4a22caa92f010f59
anohemi223 <anohemi223@gmail.com>
2025-12-19 15:00:03 -0800

    Change collectorNumber type from int to String

--- finalproject/src/finalproject/CardStats.java
+++ finalproject/src/finalproject/CardStats.java
@@ -1,22 +1,27 @@
 /**
+ * Lead Author(s):
  * 
+ * @author Nohemi Arquette
+ * 
+ *         Responsibilities: Represents statistical details for a Pokémon card.
  */
+
 public class CardStats {
 	private String setName;
 	private String rarity;
 	private double price;
-	private int collectorNumber;
+	private String collectorNumber;
 
 	/**
 	 * Constructs a CardStats object with a set name, rarity, price, and collector
 	 * number.
 	 *
-	 * @param setName         the name of the card set
-	 * @param rarity          the rarity of the card
-	 * @param price           the market price of the card
-	 * @param collectorNumber the unique collector number within the set
+	 * @param setName          the name of the card set
+	 * @param rarity           the rarity of the card
+	 * @param price            the market price of the card
+	 * @param collectorNumber2 the unique collector number within the set
 	 */
-	public CardStats(String setName, String rarity, double price, int collectorNumber) {
+	public CardStats(String setName, String rarity, double price, String collectorNumber) {
 		this.setName = setName;
 		this.rarity = rarity;
 		this.price = price;
@@ -32,28 +37,31 @@ public class CardStats {
 	public String getSetName() {
 		return setName;
 	}
-	 /**
-     * Returns the rarity of the card.
-     *
-     * @return the card rarity
-     */
+
+	/**
+	 * Returns the rarity of the card.
+	 *
+	 * @return the card rarity
+	 */
 	public String getRarity() {
 		return rarity;
 	}
+
 	/**
-     * Returns the market price of the card.
-     *
-     * @return the price
-     */
+	 * Returns the market price of the card.
+	 *
+	 * @return the price
+	 */
 	public double getPrice() {
 		return price;
 	}
-	  /**
-     * Returns the collector number of the card.
-     *
-     * @return the collector number
-     */
-	public int getCollectorNumber() {
+
+	/**
+	 * Returns the collector number of the card.
+	 *
+	 * @return the collector number
+	 */
+	public String getCollectorNumber() {
 		return collectorNumber;
 	}
 
@@ -66,31 +74,44 @@ public class CardStats {
 	public void setSetName(String setName) {
 		this.setName = setName;
 	}
+
 	/**
-     * Sets the rarity of the card.
-     *
-     * @param rarity the new rarity
-     */
+	 * Sets the rarity of the card.
+	 *
+	 * @param rarity the new rarity
+	 */
 	public void setRarity(String rarity) {
 		this.rarity = rarity;
 	}
+
 	/**
-     * Sets the market price of the card.
-     *
-     * @param price the new price
-     */
+	 * Sets the market price of the card.
+	 *
+	 * @param price the new price
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	/**
-     * Sets the collector number of the card.
-     *
-     * @param collectorNumber the new collector number
-     */
-	public void setCollectorNumber(int collectorNumber) {
+	 * Sets the collector number of the card.
+	 *
+	 * @param collectorNumber the new collector number
+	 */
+	public void setCollectorNumber(String collectorNumber) {
 		this.collectorNumber = collectorNumber;
 	}
 
+	/**
+	 * Returns a string representation of the card stats.
+	 *
+	 * @return a string containing rarity, set name, and price
+	 */
+	@Override
+	public String toString() {
+		return rarity + " (" + setName + ") - $" + price;
+	}
+}
 	@Override
 	public String toString() {
 		return rarity + " (" + setName + ") - $" + price;
