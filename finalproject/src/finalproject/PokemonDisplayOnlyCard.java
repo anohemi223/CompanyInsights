53289b0c7181c2af10578a6c6cd924006027e207
anohemi223 <anohemi223@gmail.com>
2025-12-19 15:03:35 -0800

    Enhance documentation for PokemonDisplayOnlyCard
    Updated constructor and methods with detailed Javadoc comments.

--- finalproject/src/finalproject/PokemonDisplayOnlyCard.java
+++ finalproject/src/finalproject/PokemonDisplayOnlyCard.java
@@ -1,17 +1,45 @@
+/**
+ * Lead Author(s):
+ * 
+ * @author Nohemi Arquette
+ * 
+ *         Responsibilities: Represents a "display only" Pokémon card used for
+ *         visual collection purposes.
+ */
+
+// PokemonDisplayOnlyCard IS-A PokemonCard
 public class PokemonDisplayOnlyCard extends PokemonCard {
-    private String displayType;
+	private String displayType;
 
-    public PokemonDisplayOnlyCard(String name, String collectorNumber, String type) throws InvalidInputException {
-        super(name, collectorNumber, "Display");
-        this.displayType = type;
-    }
+	/**
+	 * Constructs a display only Pokémon card.
+	 *
+	 * @param name            the Pokémon card name
+	 * @param collectorNumber the card's collector number
+	 * @param type            the display type (Art, Jumbo, ect.)
+	 * @throws InvalidInputException if inputs are invalid
+	 */
+	public PokemonDisplayOnlyCard(String name, String collectorNumber, String type) throws InvalidInputException {
+		super(name, collectorNumber, "Display");
+		this.displayType = type;
+	}
 
-    public String getDisplayType() {
-        return displayType;
-    }
+	/**
+	 * Returns the display type of this card.
+	 *
+	 * @return display type
+	 */
+	public String getDisplayType() {
+		return displayType;
+	}
 
-    @Override
-    public String toString() {
-        return getCategory() + " Card: " + getName() + " (#" + getCollectorNumber() + ") - Type: " + displayType;
-    }
+	/**
+	 * Returns a string representation of the display-only card.
+	 *
+	 * @return string describing the card
+	 */
+	@Override
+	public String toString() {
+		return getCategory() + " Card: " + getName() + " (#" + getCollectorNumber() + ") - Type: " + displayType;
+	}
 }
