7c1a8a7cd1cbdd7e4945b562234f123561d4caba
anohemi223 <anohemi223@gmail.com>
2025-12-19 15:02:58 -0800

    Refactor PokemonCard to add category and improve methods
    Updated PokemonCard class to include category and improved methods.

--- finalproject/src/finalproject/PokemonCard.java
+++ finalproject/src/finalproject/PokemonCard.java
@@ -1,50 +1,86 @@
-/**
- * 
- */
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Lead Author(s):
+ * 
+ * @author Nohemi Arquette
+ * 
+ *         Responsibilities: Represents a playable Pokémon trading card with
+ *         imported statistics.
+ */
+
+//PokemonCard IS-A TradingCard
+//PokemonCard HAS-A List<CardStats>
 public class PokemonCard extends TradingCard {
-    private List<CardStats> stats;
+	private List<CardStats> stats;
+	private String category;
 
-    public PokemonCard(String name, int collectorNumber) {
-        super(name, collectorNumber);
-        this.stats = new ArrayList<>();
-    }
-
-
-    public List<CardStats> getStats() {
-        return stats;
-    }
-
-    // Add / update cards
-    public void addOrUpdateStats(CardStats newStats) {
-        for (int i = 0; i < stats.size(); i++) {
-            CardStats currentStats  = stats.get(i);
-            if (currentStats.getCollectorNumber() == newStats.getCollectorNumber()) {
-                currentStats.setPrice(newStats.getPrice());
-                currentStats.setRarity(newStats.getRarity());
-                currentStats.setSetName(newStats.getSetName());
-                return;
-            }
-        }
-        stats.add(newStats);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder(getCategory() + " Card: " + getName() + " (#" + getCollectorNumber() + ")\n");
-        for (int i = 0; i < stats.size(); i++) {
-            sb.append("   ").append(stats.get(i).toString()).append("\n");
-        }
-        return sb.toString();
-    }
-
-    @Override
-		public String getCategory() {
-		    return "Pokémon";
-		}
-
-		
+	/**
+	 * Constructs a PokemonCard with a name, collector number, and category.
+	 *
+	 * @param name            the Pokémon card name
+	 * @param collectorNumber the card's collector number
+	 * @param category        the card's category
+	 * @throws InvalidInputException if inputs are invalid
+	 */
+	public PokemonCard(String name, String collectorNumber, String category) throws InvalidInputException {
+		super(name, collectorNumber);
+		this.category = category;
+		this.stats = new ArrayList<>();
 	}
+
+	/**
+	 * Returns the list of CardStats for the specified card.
+	 *
+	 * @return List of CardStats
+	 */
+	public List<CardStats> getStats() {
+		return stats;
+	}
+
+	/**
+	 * Adds a new CardStats or updates existing statistics. Statistics are matched
+	 * by collector number.
+	 *
+	 * @param newStats the CardStats to add or update
+	 */
+	public void addOrUpdateStats(CardStats newStats) {
+		for (int i = 0; i < stats.size(); i++) {
+			CardStats currentStats = stats.get(i);
+			if (currentStats.getCollectorNumber().equals(newStats.getCollectorNumber())) {
+				currentStats.setPrice(newStats.getPrice());
+				currentStats.setRarity(newStats.getRarity());
+				currentStats.setSetName(newStats.getSetName());
+				return;
+			}
+		}
+		stats.add(newStats);
+	}
+
+	/**
+	 * Returns the category of card.
+	 *
+	 * @return the category as a String
+	 */
+	@Override
+	public String getCategory() {
+		return category;
+	}
+
+	/**
+	 * Returns a string representation of the card and its statistics.
+	 *
+	 * @return string of the card
+	 */
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(
+				getCategory() + " Card: " + getName() + " (#" + getCollectorNumber() + ")\n");
+		for (int i = 0; i < stats.size(); i++) {
+			sb.append("   ").append(stats.get(i).toString()).append("\n");
+		}
+		return sb.toString();
+	}
+}
