e4c2f3104392cced9bb76ce7f5eca144b2199920
anohemi223 <anohemi223@gmail.com>
2025-12-19 15:04:29 -0800

    Refactor Trainer class for consistency and clarity

--- finalproject/src/finalproject/Trainer.java
+++ finalproject/src/finalproject/Trainer.java
@@ -1,61 +1,68 @@
-import java.util.Scanner;
 
 /**
- * Represents a Pokémon Trainer with a name, favorite Pokemon , and collection of cards.
+ * Lead Author(s):
+ * 
+ * @author Nohemi Arquette
+ * 
+ *         Responsibilities: Represents a Pokémon Trainer with a name, favorite
+ *         Pokemon , and collection of cards.
  */
+
+// Trainer HAS-A Collection Manager
 public class Trainer {
-    private String name;
-    private CollectionManager collection;
+	private String name;
+	private CollectionManager collection;
 	private String favPokemon;
 
-    /**
-     *Trainer w/ name and favorite pokemon
-     * @param name  the trainer's name
-     * @param favPokemon the trainer's favorite Pokemon
-     */
-    public Trainer(String name, String favPokemon) {
-        this.name = name;
-        this.favPokemon = favPokemon;
-        this.collection = new CollectionManager();
-    }
+	/**
+	 * Trainer w/ name and favorite Pokémon
+	 * 
+	 * @param name       the trainer's name
+	 * @param favPokemon the trainer's favorite Pokémon
+	 */
+	public Trainer(String name, String favPokemon) {
+		this.name = name;
+		this.favPokemon = favPokemon;
+		this.collection = new CollectionManager();
+	}
 
-    /**
-     * @return the trainer's name
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * @return the trainer's name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * @return the trainer's email
-     */
-    public String getFavPokemon() {
-        return favPokemon;
-    }
+	/**
+	 * @return the trainer's favorite Pokémon
+	 */
+	public String getFavPokemon() {
+		return favPokemon;
+	}
 
-    /**
-     * @return the trainer's collection manager
-     */
-    public CollectionManager getCollection() {
-        return collection;
-    }
+	/**
+	 * @return the trainer's collection manager
+	 */
+	public CollectionManager getCollection() {
+		return collection;
+	}
 
-    /**
-     * Adds a card to the trainer's collection.
-     *
-     * @param card the Pokémon card to add
-     */
-    public void addCardToCollection(PokemonCard card) {
-        collection.addCard(card);
-    }
+	/**
+	 * Adds a card to the trainer's collection.
+	 *
+	 * @param card the Pokémon card to add
+	 */
+	public void addCardToCollection(PokemonCard card) {
+		collection.addCard(card);
+	}
 
-    /**
-     * Displays the trainer's information and all cards in their collection.
-     */
-    public void displayTrainerInfo() {
-        System.out.println("Trainer: " + name + " | Favorite Pokemon" + favPokemon);
-        System.out.println("Collection:");
-        collection.displayCollection();
-    }
+	/**
+	 * Displays the trainer's information and all cards in their collection.
+	 */
+	public void displayTrainerInfo() {
+		System.out.println("Trainer: " + name + " | Favorite Pokemon: " + favPokemon);
+		System.out.println("Collection:");
+		collection.displayCollection();
+	}
 }
 
