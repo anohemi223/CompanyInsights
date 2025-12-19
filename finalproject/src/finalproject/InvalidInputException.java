f116257ddb9d630dc03ce13490cff3a75700f4be
anohemi223 <anohemi223@gmail.com>
2025-12-19 15:01:39 -0800

    Update InvalidInputException.java

--- finalproject/src/finalproject/InvalidInputException.java
+++ finalproject/src/finalproject/InvalidInputException.java
@@ -1,6 +1,21 @@
+/**
+ * Lead Author(s):
+ * 
+ * @author Nohemi Arquette
+ * 
+ *         Responsibilities: Custom exception class to handle invalid user input
+ *         for Pokémon cards.
+ */
+
+// InvalidInputException IS-A Exception
 public class InvalidInputException extends Exception {
 
-	 public InvalidInputException(String message) {
-	        super(message);
-	    }
+	/**
+	 * Constructs an InvalidInputException with a specific error message.
+	 *
+	 * @param message the error message
+	 */
+	public InvalidInputException(String message) {
+		super(message);
+	}
 }
